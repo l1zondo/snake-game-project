71597874c257218651b10a282fb4cc0b73976721
l1zondo <lizondofrancoezequiel@gmail.com>
2023-03-06 19:19:44 -0300

    agrego de css y javascript

---
+++ index.js
@@ -0,0 +1,58 @@
+const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5, snakeY = 5;
+let velocityX = 0, velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
+
+//Get high score from local storage
+
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
+
+//Pass a random between 1 and 30 as food position
+
+const updateFoodPosition = () =>{
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const handleGameOver = () =>{
+    clearInterval(setIntervalId);
+    alert("Game Over! Press Ok to replay...");
+    location.reload();
+}
+
+//change velocity value based on key press
+
+const changeDirection = e =>{
+    if(e.key === "ArrowUp" && velocityY !=1){
+        velocityX = 0;
+        velocityY = -1;
+    }else if(e.key === "ArrowDown" && velocityY != -1){
+        velocityX = 0;
+        velocityY = -1;
+    }else if(e.key === "ArrowLeft" && velocityX != 1){
+        velocityX = -1;
+        velocityY = 0;
+    }else if(e.key === "ArrowRight" && velocity != -1){
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+
+//Change Direction on each key click
+
+controls.forEach(button => button.addEventListener("click", () => changeDirection({key: button.dataset.key})));
+
+const initGame = () =>{
+    if (gameOver) return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+}
